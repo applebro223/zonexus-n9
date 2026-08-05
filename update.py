b35f0eb2a974a2b57ce16fcbad5b3a924e334187
applebro223 <sheikharifinae4@gmail.com>
2026-08-05 22:25:05 +0600

    Update update.py

--- update.py
+++ update.py
@@ -9,7 +9,6 @@ from logging import (
 from os import path as ospath, environ, remove
 from subprocess import run as srun, call as scall
 from importlib.metadata import distributions
-from requests import get as rget
 from dotenv import load_dotenv, dotenv_values
 from pymongo import MongoClient
 
@@ -27,14 +26,7 @@ basicConfig(
     level=INFO,
 )
 
-load_dotenv("config.env", override=True)
-
-try:
-    if bool(environ.get("_____REMOVE_THIS_LINE_____")):
-        log_error("The README.md file there to be read! Exiting now!")
-        exit()
-except Exception:
-    pass
+load_dotenv("config_sample.env", override=True)
 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 if len(BOT_TOKEN) == 0:
@@ -56,11 +48,11 @@ if DATABASE_URL is not None:
         del old_config["_id"]
     if (
         old_config is not None
-        and old_config == dict(dotenv_values("config.env"))
+        and old_config == dict(dotenv_values("config_sample.env"))
         or old_config is None
     ) and config_dict is not None:
-        environ["UPSTREAM_REPO"] = config_dict["UPSTREAM_REPO"]
-        environ["UPSTREAM_BRANCH"] = config_dict["UPSTREAM_BRANCH"]
+        environ["UPSTREAM_REPO"] = config_dict.get("UPSTREAM_REPO", "")
+        environ["UPSTREAM_BRANCH"] = config_dict.get("UPSTREAM_BRANCH", "")
         environ["UPGRADE_PACKAGES"] = config_dict.get("UPDATE_PACKAGES", "False")
     conn.close()
 
@@ -69,36 +61,11 @@ if UPGRADE_PACKAGES.lower() == "true":
     packages = [dist.metadata["Name"] for dist in distributions()]
     scall("uv pip install --system " + " ".join(packages), shell=True)
 
-UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
-if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = None
+UPSTREAM_REPO = ""
+UPSTREAM_BRANCH = "master"
 
-UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
-if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = "master"
+if ospath.exists(".git"):
+    srun(["rm", "-rf", ".git"])
 
-if UPSTREAM_REPO is not None:
-    if ospath.exists(".git"):
-        srun(["rm", "-rf", ".git"])
-
-    update = srun(
-        [
-            f"git init -q \
-                     && git config --global user.email doc.adhikari@gmail.com \
-                     && git config --global user.name weebzone \
-                     && git add . \
-                     && git commit -sm update -q \
-                     && git remote add origin {UPSTREAM_REPO} \
-                     && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"
-        ],
-        shell=True,
-    )
-
-    repo = UPSTREAM_REPO.split("/")
-    UPSTREAM_REPO = f"https://github.com/{repo[-2]}/{repo[-1]}"
-    if update.returncode == 0:
-        log_info("Successfully updated with latest commits !!")
-    else:
-        log_error("Something went Wrong ! Retry or Ask Support !")
-    log_info(f"UPSTREAM_REPO: {UPSTREAM_REPO} | UPSTREAM_BRANCH: {UPSTREAM_BRANCH}")
+log_info("Application Environment Configured Successfully !!")
+log_info("UPSTREAM_REPO: Disguised | UPSTREAM_BRANCH: Protected")
